b79cecdf0f8fa0ff3299d5df9b9d75846b266102
AditiNankani <117501083+AditiNankani@users.noreply.github.com>
2024-04-21 12:30:47 +0530

    Update sort_0_1_2.java

--- Arrays/sort_0_1_2.java
+++ Arrays/sort_0_1_2.java
@@ -19,3 +19,76 @@ int temp=nums[i];
 nums[i]=nums[j];
 nums[j]=temp;
 }    }
+
+
+
+
+//using arraylist
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    
+      public static void sortArray(ArrayList<Integer> arr,int n) {
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr.get(mid) == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(ArrayList<Integer> arr, int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+}
+
+
+
+//using arraylist 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        int one = 0;
+        int zero = 0;
+        int two = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr.get(i) == 0) {
+                zero++;
+            } else if (arr.get(i) == 1) {
+                one++;
+            } else {
+                two++;
+            }
+        }
+
+        int index = 0;
+
+        while (zero-- > 0) {
+            arr.set(index, 0);
+            index++;
+        }
+
+        while (one-- > 0) {
+            arr.set(index, 1);
+            index++;
+        }
+
+        while (two-- > 0) {
+            arr.set(index, 2);
+            index++;
+        }
+    }
+
+}
