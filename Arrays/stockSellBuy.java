2a382f5d99e3ec87d3dc3a1806673eb90db85d38
AditiNankani <117501083+AditiNankani@users.noreply.github.com>
2024-07-21 19:40:45 +0530

    Update stockSellBuy.java

--- Arrays/stockSellBuy.java
+++ Arrays/stockSellBuy.java
@@ -15,3 +15,22 @@ class Solution {
     }
        
 }
+
+
+
+
+//solution-2:
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j]>prices[i]){
+                    profit=Math.max(prices[j]-prices[i],profit);
+                }
+            }
+        }
+        return profit;
+    }
+}
